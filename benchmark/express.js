bb7d42e56a16ae25c0464340f9acb7baa422d78e
Shkumbin Maksuti <shkumbinmaksuti@gmail.com>
2017-11-25 19:15:04 +0100

    Updated benchmark files to exclude view extension

--- benchmark/express.js
+++ benchmark/express.js
@@ -8,7 +8,7 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.render('../../templates/index.ejs', { text: 'text' })
+  res.render('../../templates/index', { text: 'text' })
 })
 
 app.listen(3000, err => {
