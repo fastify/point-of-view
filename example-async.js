434910ccac32c4d8157815912da45d4731431e4d
Matteo Collina <hello@matteocollina.com>
2019-12-11 13:57:33 +0100

    async example (#139)
    * async example
    
    * Fix template

---
+++ example-async.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const { promisify } = require('util')
+const sleep = promisify(setTimeout)
+const templates = 'templates'
+
+const fastify = require('fastify')({
+  logger: true
+})
+
+fastify.register(require('.'), {
+  engine: {
+    nunjucks: require('nunjucks')
+  },
+  templates
+})
+
+async function something () {
+  await sleep(1000)
+  return new Date()
+}
+
+fastify.get('/', async (req, reply) => {
+  const t = await something()
+  reply.view('/index.njk', { text: t })
+  return reply
+})
+
+fastify.listen(3000, err => {
+  if (err) throw err
+  console.log(`server listening on ${fastify.server.address().port}`)
+})
