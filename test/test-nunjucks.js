743032928f7eb50d289c66b3bb7eecf28562b997
Paweł Mrowiec <work@pawel-mrowiec.pl>
2020-05-27 00:41:15 +0200

    Add ability to override defaultContext properties with ones from reply locals

--- test/test-nunjucks.js
+++ test/test-nunjucks.js
@@ -24,12 +24,11 @@ test('reply.view with nunjucks engine and custom templates folder', t => {
   fastify.register(require('../index'), {
     engine: {
       nunjucks: nunjucks
-    },
-    templates: 'templates'
+    }
   })
 
   fastify.get('/', (req, reply) => {
-    reply.view('./index.njk', data)
+    reply.view('./templates/index.njk', data)
   })
 
   fastify.listen(0, err => {
@@ -43,8 +42,7 @@ test('reply.view with nunjucks engine and custom templates folder', t => {
       t.strictEqual(response.statusCode, 200)
       t.strictEqual(response.headers['content-length'], '' + body.length)
       t.strictEqual(response.headers['content-type'], 'text/html; charset=utf-8')
-      // Global Nunjucks templates dir changed here.
-      t.strictEqual(nunjucks.render('./index.njk', data), body.toString())
+      t.strictEqual(nunjucks.render('./templates/index.njk', data), body.toString())
       fastify.close()
     })
   })
@@ -60,12 +58,11 @@ test('reply.view for nunjucks engine without data-parameter but defaultContext',
     engine: {
       nunjucks: nunjucks
     },
-    templates: 'templates',
     defaultContext: data
   })
 
   fastify.get('/', (req, reply) => {
-    reply.view('./index.njk')
+    reply.view('./templates/index.njk')
   })
 
   fastify.listen(0, err => {
@@ -79,8 +76,7 @@ test('reply.view for nunjucks engine without data-parameter but defaultContext',
       t.strictEqual(response.statusCode, 200)
       t.strictEqual(response.headers['content-length'], '' + body.length)
       t.strictEqual(response.headers['content-type'], 'text/html; charset=utf-8')
-      // Global Nunjucks templates dir changed here.
-      t.strictEqual(nunjucks.render('./index.njk', data), body.toString())
+      t.strictEqual(nunjucks.render('./templates/index.njk', data), body.toString())
       fastify.close()
     })
   })
@@ -94,12 +90,11 @@ test('reply.view for nunjucks engine without data-parameter and without defaultC
   fastify.register(require('../index'), {
     engine: {
       nunjucks: nunjucks
-    },
-    templates: 'templates'
+    }
   })
 
   fastify.get('/', (req, reply) => {
-    reply.view('./index.njk')
+    reply.view('./templates/index.njk')
   })
 
   fastify.listen(0, err => {
@@ -113,8 +108,165 @@ test('reply.view for nunjucks engine without data-parameter and without defaultC
       t.strictEqual(response.statusCode, 200)
       t.strictEqual(response.headers['content-length'], '' + body.length)
       t.strictEqual(response.headers['content-type'], 'text/html; charset=utf-8')
-      // Global Nunjucks templates dir changed here.
-      t.strictEqual(nunjucks.render('./index.njk'), body.toString())
+      t.strictEqual(nunjucks.render('./templates/index.njk'), body.toString())
+      fastify.close()
+    })
+  })
+})
+
+test('reply.view for nunjucks engine without data-parameter and defaultContext but with reply.locals', t => {
+  t.plan(6)
+  const fastify = Fastify()
+  const nunjucks = require('nunjucks')
+  const localsData = { text: 'text from locals' }
+
+  fastify.register(require('../index'), {
+    engine: {
+      nunjucks: nunjucks
+    }
+  })
+
+  fastify.addHook('preHandler', function (request, reply, done) {
+    reply.locals = localsData
+    done()
+  })
+
+  fastify.get('/', (req, reply) => {
+    reply.view('./templates/index.njk')
+  })
+
+  fastify.listen(0, err => {
+    t.error(err)
+
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 200)
+      t.strictEqual(response.headers['content-length'], '' + body.length)
+      t.strictEqual(response.headers['content-type'], 'text/html; charset=utf-8')
+      t.strictEqual(nunjucks.render('./templates/index.njk', localsData), body.toString())
+      fastify.close()
+    })
+  })
+})
+
+test('reply.view for nunjucks engine without defaultContext but with reply.locals and data-parameter', t => {
+  t.plan(6)
+  const fastify = Fastify()
+  const nunjucks = require('nunjucks')
+  const localsData = { text: 'text from locals' }
+  const data = { text: 'text' }
+
+  fastify.register(require('../index'), {
+    engine: {
+      nunjucks: nunjucks
+    }
+  })
+
+  fastify.addHook('preHandler', function (request, reply, done) {
+    reply.locals = localsData
+    done()
+  })
+
+  fastify.get('/', (req, reply) => {
+    reply.view('./templates/index.njk', data)
+  })
+
+  fastify.listen(0, err => {
+    t.error(err)
+
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 200)
+      t.strictEqual(response.headers['content-length'], '' + body.length)
+      t.strictEqual(response.headers['content-type'], 'text/html; charset=utf-8')
+      t.strictEqual(nunjucks.render('./templates/index.njk', data), body.toString())
+      fastify.close()
+    })
+  })
+})
+
+test('reply.view for nunjucks engine without data-parameter but with reply.locals and defaultContext', t => {
+  t.plan(6)
+  const fastify = Fastify()
+  const nunjucks = require('nunjucks')
+  const localsData = { text: 'text from locals' }
+  const contextData = { text: 'text from context' }
+
+  fastify.register(require('../index'), {
+    engine: {
+      nunjucks: nunjucks
+    },
+    defaultContext: contextData
+  })
+
+  fastify.addHook('preHandler', function (request, reply, done) {
+    reply.locals = localsData
+    done()
+  })
+
+  fastify.get('/', (req, reply) => {
+    reply.view('./templates/index.njk')
+  })
+
+  fastify.listen(0, err => {
+    t.error(err)
+
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 200)
+      t.strictEqual(response.headers['content-length'], '' + body.length)
+      t.strictEqual(response.headers['content-type'], 'text/html; charset=utf-8')
+      t.strictEqual(nunjucks.render('./templates/index.njk', localsData), body.toString())
+      fastify.close()
+    })
+  })
+})
+
+test('reply.view for nunjucks engine with data-parameter and reply.locals and defaultContext', t => {
+  t.plan(6)
+  const fastify = Fastify()
+  const nunjucks = require('nunjucks')
+  const localsData = { text: 'text from locals' }
+  const contextData = { text: 'text from context' }
+  const data = { text: 'text' }
+
+  fastify.register(require('../index'), {
+    engine: {
+      nunjucks: nunjucks
+    },
+    defaultContext: contextData
+  })
+
+  fastify.addHook('preHandler', function (request, reply, done) {
+    reply.locals = localsData
+    done()
+  })
+
+  fastify.get('/', (req, reply) => {
+    reply.view('./templates/index.njk', data)
+  })
+
+  fastify.listen(0, err => {
+    t.error(err)
+
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 200)
+      t.strictEqual(response.headers['content-length'], '' + body.length)
+      t.strictEqual(response.headers['content-type'], 'text/html; charset=utf-8')
+      t.strictEqual(nunjucks.render('./templates/index.njk', data), body.toString())
       fastify.close()
     })
   })
