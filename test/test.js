0bde9aa73bbd7d48df6838b70280c324f087a08b
Alex Smith <alex.smith2844@gmail.com>
2021-02-01 16:37:02 +0500

    Default extension for ejs (#205)
    * Default extension for ejs
    
    * Default extension for ejs

--- test/test.js
+++ test/test.js
@@ -168,6 +168,39 @@ test('reply.view can be returned from async function to indicate response proces
   })
 })
 
+test('Default extension for ejs', t => {
+  t.plan(6)
+  const fastify = Fastify()
+
+  fastify.register(require('../index'), {
+    engine: {
+      ejs: require('ejs')
+    },
+    root: path.join(__dirname, '../templates'),
+    viewExt: 'html'
+  })
+
+  fastify.get('/', async (req, reply) => {
+    return reply.view('index-with-includes-without-ext')
+  })
+
+  fastify.listen(0, err => {
+    t.error(err)
+
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 200)
+      t.strictEqual(response.headers['content-length'], '' + body.length)
+      t.strictEqual(response.headers['content-type'], 'text/html; charset=utf-8')
+      t.strictEqual('ok', body.toString().trim())
+      fastify.close()
+    })
+  })
+})
+
 test('reply.view with ejs engine and custom propertyName', t => {
   t.plan(11)
   const fastify = Fastify()
