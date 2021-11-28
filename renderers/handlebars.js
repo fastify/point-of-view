cf8ca99c128d369ee2195ba62a1d607f920904aa
Jonas Galvez <jonasgalvez@gmail.com>
2021-11-28 05:24:35 -0300

    wip renderer refactor proposal

--- renderers/handlebars.js
+++ renderers/handlebars.js
@@ -1,1 +1,51 @@
-handlebars.js
+function viewHandlebars (page, data) {
+  if (!page) {
+    this.send(new Error('Missing page'))
+    return
+  }
+
+  const options = Object.assign({}, opts.options)
+  data = Object.assign({}, defaultCtx, this.locals, data)
+  // append view extension
+  page = getPage(page, 'hbs')
+  getTemplate(page, (err, template) => {
+    if (err) {
+      this.send(err)
+      return
+    }
+
+    if (prod) {
+      try {
+        const html = template(data)
+        if (!this.getHeader('content-type')) {
+          this.header('Content-Type', 'text/html; charset=' + charset)
+        }
+        this.send(html)
+      } catch (e) {
+        this.send(e)
+      }
+    } else {
+      getPartials(type, options.partials || {}, (err, partialsObject) => {
+        if (err) {
+          this.send(err)
+          return
+        }
+
+        try {
+          Object.keys(partialsObject).forEach((name) => {
+            engine.registerPartial(name, engine.compile(partialsObject[name]))
+          })
+
+          const html = template(data)
+
+          if (!this.getHeader('content-type')) {
+            this.header('Content-Type', 'text/html; charset=' + charset)
+          }
+          this.send(html)
+        } catch (e) {
+          this.send(e)
+        }
+      })
+    }
+  })
+}
--- renderers/marko.js
+++ renderers/marko.js
@@ -1,1 +1,37 @@
-marko.js
+function viewMarko (page, data, opts) {
+  if (!page) {
+    this.send(new Error('Missing page'))
+    return
+  }
+
+  data = Object.assign({}, defaultCtx, this.locals, data)
+  // append view extension
+  page = getPage(page, type)
+
+  // Support compile template from memory
+  // opts.templateSrc : string - pre-loaded template source
+  // even to load from memory, a page parameter still should be provided and the parent path should exist for the loader to search components along the path.
+
+  const template = opts && opts.templateSrc ? engine.load(join(templatesDir, page), opts.templateSrc) : engine.load(join(templatesDir, page))
+
+  if (opts && opts.stream) {
+    if (typeof options.useHtmlMinifyStream === 'function') {
+      this.send(template.stream(data).pipe(options.useHtmlMinifyStream(options.htmlMinifierOptions || {})))
+    } else {
+      this.send(template.stream(data))
+    }
+  } else {
+    template.renderToString(data, send(this))
+  }
+
+  function send (that) {
+    return function _send (err, html) {
+      if (err) return that.send(err)
+      if (options.useHtmlMinifier && (typeof options.useHtmlMinifier.minify === 'function')) {
+        html = options.useHtmlMinifier.minify(html, options.htmlMinifierOptions || {})
+      }
+      that.header('Content-Type', 'text/html; charset=' + charset)
+      that.send(html)
+    }
+  }
+}
