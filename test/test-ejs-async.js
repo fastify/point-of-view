3be77d23bf7add8bd8c19f2b1606c8e74cd724aa
Александр Абрамов <ttpo100ajiex@mail.ru>
2022-05-27 13:06:00 +0300

    Support for ejs rendering with async: true in production mode Without proper handling, asynchronous templates are rendered correctly only (once) while they are not saved in the cache. If the page is not rendered from scratch but is rather retrieved from cache, we must also await it before sending to the client if async is set to true.

--- test/test-ejs-async.js
+++ test/test-ejs-async.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const t = require('tap')
 const test = t.test
 const sget = require('simple-get').concat
@@ -30,8 +32,45 @@ test('reply.view with ejs engine and async: true', t => {
       t.equal(response.statusCode, 200)
       t.equal(response.headers['content-length'], '' + body.length)
       t.equal(response.headers['content-type'], 'text/html; charset=utf-8')
-      t.equal(await ejs.render(fs.readFileSync('./templates/ejs-async.ejs', 'utf8'), {}, { async: true }), body.toString())
+      const correctResult = await ejs.render(fs.readFileSync('./templates/ejs-async.ejs', 'utf8'), {}, { async: true })
+      t.equal(correctResult, body.toString())
       fastify.close()
     })
+  })
+})
+
+test('reply.view with ejs engine, async: true, and production: true', t => {
+  const numTests = 4
+  t.plan(numTests * 5 + 1)
+  const fastify = Fastify()
+  const ejs = require('ejs')
+
+  fastify.register(require('../index'), {
+    engine: { ejs },
+    production: true,
+    options: { async: true },
+    templates: 'templates'
+  })
+
+  fastify.get('/', (req, reply) => {
+    reply.view('ejs-async.ejs')
+  })
+
+  fastify.listen({ port: 0 }, err => {
+    t.error(err)
+
+    for (let i = 0; i < numTests; i++) {
+      sget({
+        method: 'GET',
+        url: 'http://localhost:' + fastify.server.address().port
+      }, async (err, response, body) => {
+        t.error(err)
+        t.equal(response.statusCode, 200)
+        t.equal(response.headers['content-length'], '' + body.length)
+        t.equal(response.headers['content-type'], 'text/html; charset=utf-8')
+        t.equal(await ejs.render(fs.readFileSync('./templates/ejs-async.ejs', 'utf8'), {}, { async: true }), body.toString())
+        if (i === numTests - 1) fastify.close()
+      })
+    }
   })
 })
