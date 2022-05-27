8858c02a672db298fc414f5c51341b0f7e265d85
Александр Абрамов <ttpo100ajiex@mail.ru>
2022-05-27 13:37:39 +0300

    Better testing of async rendering with ejs

--- test/test-ejs-async.js
+++ test/test-ejs-async.js
@@ -6,7 +6,7 @@ const sget = require('simple-get').concat
 const Fastify = require('fastify')
 const fs = require('fs')
 
-test('reply.view with ejs engine and async: true', t => {
+test('reply.view with ejs engine and async: true (global option)', t => {
   t.plan(6)
   const fastify = Fastify()
   const ejs = require('ejs')
@@ -32,15 +32,14 @@ test('reply.view with ejs engine and async: true', t => {
       t.equal(response.statusCode, 200)
       t.equal(response.headers['content-length'], '' + body.length)
       t.equal(response.headers['content-type'], 'text/html; charset=utf-8')
-      const correctResult = await ejs.render(fs.readFileSync('./templates/ejs-async.ejs', 'utf8'), {}, { async: true })
-      t.equal(correctResult, body.toString())
+      t.equal(await ejs.render(fs.readFileSync('./templates/ejs-async.ejs', 'utf8'), {}, { async: true }), body.toString())
       fastify.close()
     })
   })
 })
 
-test('reply.view with ejs engine, async: true, and production: true', t => {
-  const numTests = 4
+test('reply.view with ejs engine, async: true (global option), and production: true', t => {
+  const numTests = 2
   t.plan(numTests * 5 + 1)
   const fastify = Fastify()
   const ejs = require('ejs')
@@ -56,20 +55,23 @@ test('reply.view with ejs engine, async: true, and production: true', t => {
     reply.view('ejs-async.ejs')
   })
 
-  fastify.listen({ port: 0 }, err => {
+  fastify.listen({ port: 0 }, async err => {
     t.error(err)
 
     for (let i = 0; i < numTests; i++) {
-      sget({
-        method: 'GET',
-        url: 'http://localhost:' + fastify.server.address().port
-      }, async (err, response, body) => {
-        t.error(err)
-        t.equal(response.statusCode, 200)
-        t.equal(response.headers['content-length'], '' + body.length)
-        t.equal(response.headers['content-type'], 'text/html; charset=utf-8')
-        t.equal(await ejs.render(fs.readFileSync('./templates/ejs-async.ejs', 'utf8'), {}, { async: true }), body.toString())
-        if (i === numTests - 1) fastify.close()
+      await new Promise((resolve, reject) => {
+        sget({
+          method: 'GET',
+          url: 'http://localhost:' + fastify.server.address().port
+        }, async (err, response, body) => {
+          t.error(err)
+          t.equal(response.statusCode, 200)
+          t.equal(response.headers['content-length'], '' + body.length)
+          t.equal(response.headers['content-type'], 'text/html; charset=utf-8')
+          t.equal(await ejs.render(fs.readFileSync('./templates/ejs-async.ejs', 'utf8'), {}, { async: true }), body.toString())
+          if (i === numTests - 1) fastify.close()
+          resolve()
+        })
       })
     }
   })
