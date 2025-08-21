f24529cc3f938741740ca81ee7ddb144b259f6de
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-08-21 10:14:26 +0200

    chore: remove tap

--- test/test-ejs-with-snapshot.js
+++ test/test-ejs-with-snapshot.js
@@ -1,7 +1,6 @@
 'use strict'
 
-const t = require('tap')
-const test = t.test
+const { test } = require('node:test')
 const Fastify = require('fastify')
 const path = require('node:path')
 const ejs = require('ejs')
@@ -11,13 +10,13 @@ const options = {
   views: [path.join(__dirname, '..')]
 }
 
-test('reply.view with ejs engine, template folder specified, include files (ejs and html) used in template, includeViewExtension property as true; requires TAP snapshots enabled', async t => {
+test('reply.view with ejs engine, template folder specified, include files (ejs and html) used in template, includeViewExtension property as true', async t => {
   t.plan(6)
   const fastify = Fastify()
 
   const data = { text: 'text' }
 
-  fastify.register(require('../../index'), {
+  fastify.register(require('../index'), {
     engine: {
       ejs
     },
@@ -36,32 +35,33 @@ test('reply.view with ejs engine, template folder specified, include files (ejs 
 
   const responseContent = await result.text()
 
-  t.equal(result.status, 200)
-  t.equal(result.headers.get('content-length'), '' + responseContent.length)
-  t.equal(result.headers.get('content-type'), 'text/html; charset=utf-8')
+  t.assert.strictEqual(result.status, 200)
+  t.assert.strictEqual(result.headers.get('content-length'), '' + responseContent.length)
+  t.assert.strictEqual(result.headers.get('content-type'), 'text/html; charset=utf-8')
 
   let content = null
 
   await new Promise(resolve => {
     ejs.renderFile(path.join(templatesFolder, 'index-linking-other-pages.ejs'), data, options, function (err, str) {
       content = str
-      t.error(err)
-      t.equal(content.length, responseContent.length)
+      t.assert.ifError(err)
+      t.assert.strictEqual(content.length, responseContent.length)
       resolve()
     })
   })
-  t.matchSnapshot(content.replace(/\r?\n/g, ''), 'output') // normalize new lines for cross-platform
+  const expectedContent = '<!DOCTYPE html><html lang="en">  <head></head>  <body>    <p>text</p>    <br/>    <div>      <p>Other EJS pages with includes:</p>      <ul>        <li>Normal page, <a href="/include-test">here</a></li>        <li>One include not exist, <a href="/include-one-include-missing-test">here</a>          (to raise errors)        </li>        <li>One attribute not exist, <a href="/include-one-attribute-missing-test">here</a>          (to raise errors)        </li>      </ul>    <div>  </body></html>'
+  t.assert.strictEqual(content.replace(/\r?\n/g, ''), expectedContent) // normalize new lines for cross-platform
 
   await fastify.close()
 })
 
-test('reply.view with ejs engine, templates with folder specified, include files and attributes; requires TAP snapshots enabled; home', async t => {
+test('reply.view with ejs engine, templates with folder specified, include files and attributes; home', async t => {
   t.plan(6)
   const fastify = Fastify()
 
   const data = { text: 'Hello from EJS Templates' }
 
-  fastify.register(require('../../index'), {
+  fastify.register(require('../index'), {
     engine: {
       ejs
     },
@@ -80,31 +80,32 @@ test('reply.view with ejs engine, templates with folder specified, include files
 
   const responseContent = await result.text()
 
-  t.equal(result.status, 200)
-  t.equal(result.headers.get('content-length'), '' + responseContent.length)
-  t.equal(result.headers.get('content-type'), 'text/html; charset=utf-8')
+  t.assert.strictEqual(result.status, 200)
+  t.assert.strictEqual(result.headers.get('content-length'), '' + responseContent.length)
+  t.assert.strictEqual(result.headers.get('content-type'), 'text/html; charset=utf-8')
 
   let content = null
   await new Promise(resolve => {
     ejs.renderFile(path.join(templatesFolder, 'index.ejs'), data, options, function (err, str) {
       content = str
-      t.error(err)
-      t.equal(content.length, responseContent.length)
+      t.assert.ifError(err)
+      t.assert.strictEqual(content.length, responseContent.length)
       resolve()
     })
   })
-  t.matchSnapshot(content.replace(/\r?\n/g, '')) // normalize new lines for cross-platform
+  const expectedContent = '<!DOCTYPE html><html lang="en">  <head></head>  <body>    <p>Hello from EJS Templates</p>    <br/>  </body></html>'
+  t.assert.strictEqual(content.replace(/\r?\n/g, ''), expectedContent) // normalize new lines for cross-platform
 
   await fastify.close()
 })
 
-test('reply.view with ejs engine, templates with folder specified, include files and attributes; requires TAP snapshots enabled; page with includes', async t => {
+test('reply.view with ejs engine, templates with folder specified, include files and attributes; page with includes', async t => {
   t.plan(6)
   const fastify = Fastify()
 
   const data = { text: 'Hello from EJS Templates' }
 
-  fastify.register(require('../../index'), {
+  fastify.register(require('../index'), {
     engine: {
       ejs
     },
@@ -123,31 +124,33 @@ test('reply.view with ejs engine, templates with folder specified, include files
 
   const responseContent = await result.text()
 
-  t.equal(result.status, 200)
-  t.equal(result.headers.get('content-length'), '' + responseContent.length)
-  t.equal(result.headers.get('content-type'), 'text/html; charset=utf-8')
+  t.assert.strictEqual(result.status, 200)
+  t.assert.strictEqual(result.headers.get('content-length'), '' + responseContent.length)
+  t.assert.strictEqual(result.headers.get('content-type'), 'text/html; charset=utf-8')
 
   let content = null
   await new Promise(resolve => {
     ejs.renderFile(path.join(templatesFolder, 'index-with-includes.ejs'), data, options, function (err, str) {
       content = str
-      t.error(err)
-      t.equal(content.length, responseContent.length)
+      t.assert.ifError(err)
+      t.assert.strictEqual(content.length, responseContent.length)
       resolve()
     })
   })
-  t.matchSnapshot(content.replace(/\r?\n/g, '')) // normalize new lines for cross-platform
+
+  const expectedContent = '<!DOCTYPE html><html lang="en">  <head></head>  <body>        <header>      Sample header (ejs)    </header>    <p>Hello from EJS Templates</p>        <footer>      Sample footer (html) - Back to <a href="/">Home</a>    </footer>  </body></html>'
+  t.assert.strictEqual(content.replace(/\r?\n/g, ''), expectedContent) // normalize new lines for cross-platform
 
   await fastify.close()
 })
 
-test('reply.view with ejs engine, templates with folder specified, include files and attributes; requires TAP snapshots enabled; page with one include missing', async t => {
+test('reply.view with ejs engine, templates with folder specified, include files and attributes; page with one include missing', async t => {
   t.plan(6)
   const fastify = Fastify()
 
   const data = { text: 'Hello from EJS Templates' }
 
-  fastify.register(require('../../index'), {
+  fastify.register(require('../index'), {
     engine: {
       ejs
     },
@@ -166,31 +169,31 @@ test('reply.view with ejs engine, templates with folder specified, include files
 
   const responseContent = await result.text()
 
-  t.equal(result.status, 500)
-  t.equal(result.headers.get('content-length'), '' + responseContent.length)
-  t.equal(result.headers.get('content-type'), 'application/json; charset=utf-8')
+  t.assert.strictEqual(result.status, 500)
+  t.assert.strictEqual(result.headers.get('content-length'), '' + responseContent.length)
+  t.assert.strictEqual(result.headers.get('content-type'), 'application/json; charset=utf-8')
 
   let content = null
   await new Promise(resolve => {
     ejs.renderFile(path.join(templatesFolder, 'index-with-includes-one-missing.ejs'), data, options, function (err, str) {
       content = str
-      t.ok(err instanceof Error)
-      t.equal(content, undefined)
+      t.assert.ok(err instanceof Error)
+      t.assert.strictEqual(content, undefined)
       resolve()
     })
   })
-  t.matchSnapshot(content)
+  t.assert.strictEqual(content, undefined)
 
   await fastify.close()
 })
 
-test('reply.view with ejs engine, templates with folder specified, include files and attributes; requires TAP snapshots enabled; page with one attribute missing', async t => {
+test('reply.view with ejs engine, templates with folder specified, include files and attributes; page with one attribute missing', async t => {
   t.plan(6)
   const fastify = Fastify()
 
   const data = { text: 'Hello from EJS Templates' }
 
-  fastify.register(require('../../index'), {
+  fastify.register(require('../index'), {
     engine: {
       ejs
     },
@@ -209,21 +212,21 @@ test('reply.view with ejs engine, templates with folder specified, include files
 
   const responseContent = await result.text()
 
-  t.equal(result.status, 500)
-  t.equal(result.headers.get('content-length'), '' + responseContent.length)
-  t.equal(result.headers.get('content-type'), 'application/json; charset=utf-8')
+  t.assert.strictEqual(result.status, 500)
+  t.assert.strictEqual(result.headers.get('content-length'), '' + responseContent.length)
+  t.assert.strictEqual(result.headers.get('content-type'), 'application/json; charset=utf-8')
 
   let content = null
   await new Promise(resolve => {
     ejs.renderFile(path.join(templatesFolder, 'index-with-includes-and-attribute-missing.ejs'), data, options, function (err, str) {
       content = str
-      t.ok(err instanceof Error)
-      t.equal(content, undefined)
+      t.assert.ok(err instanceof Error)
+      t.assert.strictEqual(content, undefined)
       resolve()
     })
   })
 
-  t.matchSnapshot(content)
+  t.assert.strictEqual(content, undefined)
 
   await fastify.close()
 })
